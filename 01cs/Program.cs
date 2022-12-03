36c76ad7e8e8818d45de8ad6c9896d786248d4bf
Daniel Kertesz <daniel@spatof.org>
2022-12-03 18:20:46 +0000

    comments and solution for part 2

--- 01cs/Program.cs
+++ 01cs/Program.cs
@@ -4,13 +4,18 @@
     {
         static void Main(string[] args)
         {
+            // 'scores' is a list of scores for each elf.
             List<int> scores = new List<int>();
+            // 'counter' contains the score of the current elf.
             int counter = 0;
+            // 'value' is a temporary variable used to store the result of parsing the number in each line.
             int value = 0;
 
+            // for each line inside the input file "input":
             foreach (string line in System.IO.File.ReadLines(@"input"))
             {
-                // check for empty lines first.
+                // check for empty lines first; when we get to an empty line we need to store the score
+                // of this elf and then continue to the next line.
                 if (line == "")
                 {
                     scores.Add(counter);
@@ -18,28 +23,37 @@
                     continue;
                 }
 
-                // try to parse the number in the current line.
+                // If we get here it means that the line wasn't empty, so we first try to parse the text
+                // into an 'int'; this is the number of calories from the current line.
                 try
                 {
                     value = Int32.Parse(line);
                 }
                 catch (FormatException)
                 {
+                    // if we get here it means that we couldn't parse the number in the current line.
                     Console.WriteLine($"cannot parse '{line}'");
                     return;
                 }
 
-                // add to the score.
+                // add the calories to the counter.
                 counter += value;
             }
 
+            // we sort the list of calories of each elf, so that our list starts with the lowest number
+            // of calories and ends with the highest.
             scores.Sort();
             Console.WriteLine($"first score: {scores[0]}, last score: {scores[scores.Count-1]}");
 
+            // Now we print the number of calories of the three elves with most calories.
+            int top3 = 0;
             for (int i = scores.Count-3; i < scores.Count; i++)
             {
                 Console.WriteLine(scores[i]);
+                top3 += scores[i];
             }
+
+            Console.WriteLine($"the sum of the calories of the top three elves is: {top3}");
         }
     }
 }
