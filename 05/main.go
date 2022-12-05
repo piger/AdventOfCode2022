7a4aa04479ccb297af31b1ddb1b5923fe604d764
Daniel Kertesz <daniel@spatof.org>
2022-12-05 12:23:39 +0000

    solution 1

--- 05/main.go
+++ 05/main.go
@@ -4,7 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
+
+var moveCmdRe = regexp.MustCompile(`^move (?P<num>\d+) from (?P<src>\d+) to (?P<dst>\d+)`)
 
 type Stack struct {
 	ID     int
@@ -19,6 +23,20 @@ func longestLine(lines []string) (longest int) {
 		}
 	}
 	return
+}
+
+func insert(a []rune, index int, value rune) []rune {
+	if len(a) == index {
+		return append(a, value)
+	}
+
+	a = append(a[:index+1], a[index:]...)
+	a[index] = value
+	return a
+}
+
+func remove(a []rune, index int) []rune {
+	return append(a[:index], a[index+1:]...)
 }
 
 func run() error {
@@ -39,6 +57,8 @@ func run() error {
 		}
 		buf = append(buf, line)
 	}
+
+	// should probably check s.Err() here?
 
 	numStacks := (longestLine(buf) + 1) / 4
 	stacks := make([]Stack, numStacks)
@@ -62,6 +82,50 @@ func run() error {
 
 	for _, stack := range stacks {
 		fmt.Printf("stack %d: %q\n", stack.ID, stack.Crates)
+	}
+
+	// read the move commands
+	for s.Scan() {
+		line := s.Text()
+		// fmt.Println(line)
+		matches := moveCmdRe.FindStringSubmatch(line)
+		idxNum := moveCmdRe.SubexpIndex("num")
+		idxSrc := moveCmdRe.SubexpIndex("src")
+		idxDst := moveCmdRe.SubexpIndex("dst")
+		// fmt.Printf("move %s items from %s to %s\n", matches[idxNum], matches[idxSrc], matches[idxDst])
+		num, err := strconv.Atoi(matches[idxNum])
+		if err != nil {
+			return err
+		}
+
+		src, err := strconv.Atoi(matches[idxSrc])
+		if err != nil {
+			return err
+		}
+
+		dst, err := strconv.Atoi(matches[idxDst])
+		if err != nil {
+			return err
+		}
+
+		// move each crate
+		for i := 0; i < num; i++ {
+			// fmt.Printf("before: %q %q\n", stacks[src-1].Crates, stacks[dst-1].Crates)
+			crate := stacks[src-1].Crates[0]
+			stacks[dst-1].Crates = insert(stacks[dst-1].Crates, 0, crate)
+			stacks[src-1].Crates = remove(stacks[src-1].Crates, 0)
+			// fmt.Printf("after: %q %q\n", stacks[src-1].Crates, stacks[dst-1].Crates)
+		}
+	}
+
+	var solution1 []rune
+	for i := range stacks {
+		solution1 = append(solution1, stacks[i].Crates[0])
+	}
+	fmt.Printf("solution 1: %s\n", string(solution1))
+
+	if err := s.Err(); err != nil {
+		return err
 	}
 
 	return nil
